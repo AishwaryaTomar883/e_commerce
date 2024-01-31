2eea8258ba0630aff40db6a92faf07589ec3e155
Aishwarya-Tomar <aishwarya.tomar@trootech.com>
2024-01-31 14:50:10 +0530

    check expiration time for token expiration

--- e_commerce/e_commerce/static/js/home.js
+++ e_commerce/e_commerce/static/js/home.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
   var accessToken = localStorage.getItem("accessToken");
   if (!accessToken) {
     $('#loginModal').modal('show');
+  } else {
+    var expirationTime = localStorage.getItem("accessTokenExpiration");
+    if (expirationTime && new Date(expirationTime) < new Date()) {
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("accessTokenExpiration");
+      $('#loginModal').modal('show');
+    }
   }
 });
 
